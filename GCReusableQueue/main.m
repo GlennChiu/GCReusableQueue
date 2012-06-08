472b6feeee6aee6aadf2f65740c7d69f39f175b1
Glenn Chiu <genoshin_gx@hotmail.com>
2012-06-08 23:13:06 +0200

    Initial Commit

---
+++ GCReusableQueue/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GCReusableQueue
+//
+//  Created by Glenn Chiu on 08-06-12.
+//  Copyright (c) 2012 Dot Square. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GCAppDelegate class]));
+    }
+}
